8a8469d609c17073552539c89bbb445ea05bb581
Emre Akkaya <emre.akkaya@agem.com.tr>
2018-07-23 17:47:00 +0300

    Postinstall scripts added

--- alfa_agent/core/base/command/command_manager.py
+++ alfa_agent/core/base/command/command_manager.py
@@ -5,14 +5,17 @@ import json
 import os
 import threading
 import logging
+import sys
+from subprocess import check_output, CalledProcessError
 from alfa_agent.core.api.system.system import System
 from alfa_agent.core.api.util.util import Util
 from alfa_agent.core.base.command.fifo import Fifo
+from alfa_agent.core.base.daemon.custom_daemon import Daemon
 
 
 class CommandManager(object):
     def __init__(self, logger=None):
-        self.commands = ['start', 'stop']
+        self.commands = ['start', 'stop', 'install']
         self.logger = logger or logging.getLogger(__name__)
 
     def set_event(self, *args):
@@ -22,9 +25,8 @@ class CommandManager(object):
                 return False
 
             params = args[0]
-            is_running = System.Agent.is_running()
             func = getattr(self, params[1].replace('-', '_'))
-            data = func(is_running, params)
+            data = func(params)
 
             if data and len(data) > 0:
                 fifo = Fifo()
@@ -35,20 +37,74 @@ class CommandManager(object):
             self.logger.error("Cannot start the agent process.", exc_info=True)
             return False
 
-    def start(self, is_running, params):
-        if is_running:
-            self.logger.warning('Agent already running')
-        else:
-            self.logger.info('Starting the agent...')
-            # TODO
-            Util.execute('Start-Process -FilePath {0} -ArgumentList "{1} _start" -WindowStyle hidden'.format(
-                System.get_python_path(), os.path.join(System.Agent.agent_dir_path(), 'agent.py')))
+    def start(self, params):
+        pass
 
-    def stop(self, is_running, params):
+    def stop(self, params):
+        pid = Daemon.get_pid()
         data = dict()
-        if is_running:
+        if pid:
             data['event'] = params[1]
         else:
-            self.logger.info('Agent already stopped.')
+            self.logger.warning('Agent already stopped.')
             data = None
         return data
+
+    def install(self, params):
+        try:
+            is_admin = os.getuid() == 0
+        except AttributeError:
+            import ctypes
+            is_admin = ctypes.windll.shell32.IsUserAnAdmin() != 0
+        if is_admin:
+            self.logger.info("Installing 3rd party dependencies...")
+            success = True
+            if sys.platform == "win32":
+                if Util.read_prop('platform_requires.win32') is not None:
+                    try:
+                        check_output("pip.exe install --no-input {}".format(
+                            " ".join(Util.read_prop('platform_requires.win32'))).split(), universal_newlines=True)
+                    except CalledProcessError as e:
+                        success = False
+                        self.logger.error("Error occurred during installation of PyWebView/Win32 dependencies: {}"
+                                          .format(str(e)))
+            elif sys.platform == "linux":
+                if is_gtk_based():
+                    if Util.read_prop('platform_requires.debian-gtk') is not None:
+                        try:
+                            check_output("apt-get install -y {}".format(" ".join(
+                                Util.read_prop('platform_requires.debian-gtk'))).split(), universal_newlines=True)
+                        except CalledProcessError as e:
+                            success = False
+                            self.logger.error("Error occurred during installation of PyWebView/GTK3.0 dependencies: {}"
+                                              .format(str(e)))
+                else: # QT-based
+                    if Util.read_prop('platform_requires.debian-qt') is not None:
+                        try:
+                            check_output("apt-get install -y {}".format(" ".join(
+                                Util.read_prop('platform_requires.debian-qt'))).split(), universal_newlines=True)
+                        except CalledProcessError as e:
+                            success = False
+                            self.logger.error("Error occurred during installation of PyWebView/Qt dependencies: {}"
+                                              .format(str(e)))
+            else:
+                self.logger.error("Failed to determine the OS! Cannot install 3rd party dependencies.")
+            self.logger.log(logging.INFO if success else logging.ERROR,
+                            "Successfully installed 3rd party dependencies" if success else
+                            "Failed to install 3rd party dependencies. Please install the following manually: {}"
+                            .format(Util.read_prop('platform_requires.win32') if sys.platform == 'win32' else
+                                                                (Util.read_prop('platform_requires.debian-gtk')
+                                                                 if is_gtk_based()
+                                                                 else Util.read_prop('platform_requires.debian-qt'))))
+        else:
+            self.logger.warning("Install command invoked without elevated privileges! "
+                                "Cannot install 3rd party dependencies.")
+
+
+def is_gtk_based():
+    try:
+        output = check_output("apt-cache policy libgtk-3-0 | grep Installed".split(), universal_newlines=True)
+        return "Installed" in output
+    except CalledProcessError as e:
+        pass
+    return False
--- setup.py
+++ setup.py
@@ -2,10 +2,6 @@ from io import open
 
 from setuptools import find_packages
 from setuptools import setup
-from setuptools.command.install import install
-from subprocess import check_output, CalledProcessError
-import sys
-import os
 
 with open('alfa_agent/__init__.py', 'r') as f:
     for line in f:
@@ -18,12 +14,6 @@ with open('alfa_agent/__init__.py', 'r') as f:
 with open('README.rst', 'r', encoding='utf-8') as f:
     readme = f.read()
 
-try:
-    is_admin = os.getuid() == 0
-except AttributeError:
-    import ctypes
-    is_admin = ctypes.windll.shell32.IsUserAnAdmin() != 0
-
 REQUIRES = [
     'pywin32 >= 1.0;platform_system=="Windows"',
     'comtypes >= 1.1;platform_system=="Windows"',
@@ -34,46 +24,6 @@ REQUIRES = [
     'elevate >= 0.1.3',
     'PyYAML >= 3.13',
 ]
-
-PLATFORM_REQUIRES = {
-    'win32-pip': ['pywin32', 'comtypes'],
-    'debian': ['lshw', 'python3-gi', 'gir1.2-webkit-3.0']
-}
-
-
-def is_gtk_based():
-    try:
-        output = check_output("apt-cache policy libgtk-3-0 | grep Installed".split(), universal_newlines=True)
-        return "Installed" in output
-    except CalledProcessError as e:
-        pass
-    return False
-
-
-class PostInstallCommand(install):
-    """Post-installation for installation mode."""
-    def run(self):
-        if is_admin:
-            if sys.platform == "win32":
-                try:
-                    check_output("pip.exe install --no-input {}".format(" ".join(PLATFORM_REQUIRES['win32-pip']))
-                                 .split(), universal_newlines=True)
-                except CalledProcessError as e:
-                    print("[ERROR] Error occurred during installation of PyWebView/Win32 dependencies: {}"
-                          .format(str(e)))
-            # Should also check for debian here...
-            elif is_gtk_based():
-                try:
-                    check_output("apt-get install -y {}".format(" ".join(PLATFORM_REQUIRES['debian'])).split(),
-                                 universal_newlines=True)
-                except CalledProcessError as e:
-                    print("[ERROR] Error occurred during installation of PyWebView/GTK3.0 dependencies: {}"
-                          .format(str(e)))
-        else:
-            print("[WARN] Install command invoked without elevated privileges! Cannot install 3rd party dependencies.\n"
-                  + "If you are installing (not building) the agent, please install following dependencies manually: {}"
-                  .format(", ".join(PLATFORM_REQUIRES["win32-pip" if sys.platform == "win32" else "debian"])))
-        install.run(self)
 
 
 # See https://blog.ionelmc.ro/presentations/packaging for more info about packaging.
@@ -119,7 +69,4 @@ setup(
         ],
     },
 
-    cmdclass={
-        'install': PostInstallCommand,
-    },
 )
